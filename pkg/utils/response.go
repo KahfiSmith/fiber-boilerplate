488bae73086d0efe73f1416fa1746420d351567c
Mohamad Al-Kahfi <91003256+KahfiSmith@users.noreply.github.com>
2026-03-07 19:54:57 +0700

    fix: update response handling to use dtoResponse for API responses

--- pkg/utils/response.go
+++ pkg/utils/response.go
@@ -1,20 +1,20 @@
 package utils
 
 import (
-	"fiber-boilerplate/pkg/models"
+	dtoResponse "fiber-boilerplate/pkg/dto/response"
 
 	"github.com/gofiber/fiber/v3"
 )
 
 func Success(c fiber.Ctx, statusCode int, data any) error {
-	return c.Status(statusCode).JSON(models.APIResponse{
+	return c.Status(statusCode).JSON(dtoResponse.APIResponse{
 		Success: true,
 		Data:    data,
 	})
 }
 
 func Error(c fiber.Ctx, statusCode int, message string, err any) error {
-	return c.Status(statusCode).JSON(models.APIResponse{
+	return c.Status(statusCode).JSON(dtoResponse.APIResponse{
 		Success: false,
 		Message: message,
 		Error:   err,
